f5283b475fbc9afe264601fa4a365397d631a08f
Khlood20 <68351216+Khlood20@users.noreply.github.com>
2020-07-29 18:47:08 +0200

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,5 +1,9 @@
 var speakWord = "Hello";
-function speak(name) {
-  console.log(speakWord + " " + name);
+ array [0]=function speak(name) {
+  console.log(speakWord + " " + names);
 }
 
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+console.log (array);
+array[0](names);
