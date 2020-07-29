e1b016b053a3d7236d02c23391cbe3265504711c
Khlood20 <68351216+Khlood20@users.noreply.github.com>
2020-07-29 21:56:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -57,10 +57,10 @@ for (var i = 0; i < names.length; i++) {
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
   if (firstLetter == 'j') {
-    window.byeSpeaker.speak(names[i]);
+    window.ByeSpeaker.speak(names[i]);
   } else {
-    window.helloSpeaker.speak(names[i]);
+    window.HelloSpeaker.speak(names[i]);
   }
 }
 
-})(window);
+}(window);
